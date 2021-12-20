4f5deb6c9046e0c0c09105f9cc44af7a7c6c9793
JonathanCh022 <chacon_022@hotmail.com>
2021-12-20 17:48:16 -0500

    incorporacion del constructor para recibir el parametro de busqueda

--- scrapymarketplace/scrapymarketplace/spiders/marketplace.py
+++ scrapymarketplace/scrapymarketplace/spiders/marketplace.py
@@ -1,13 +1,16 @@
 import scrapy
 from ..items import ScrapymarketplaceItem
 
+
 class MarketplaceSpider(scrapy.Spider):
 
     name = 'marketplace'
-    search_word = 'televisor'
 
-    start_urls = ['https://listado.mercadolibre.com.co/' + search_word]
-    user_agent = 'Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'
+    def __init__(self, *args, **kwargs):
+        super(MarketplaceSpider, self).__init__(*args, **kwargs)
+        self.search_word = kwargs.get('search_word')
+        self.start_urls = ['https://listado.mercadolibre.com.co/' + self.search_word]
+        self.user_agent = 'Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'
 
     def start_request(self, response):
 
@@ -18,5 +21,5 @@ class MarketplaceSpider(scrapy.Spider):
 
         arr = response.xpath('//div[@class="ui-search-result__image"]/a[contains(@href, "articulo.mercadolibre.com.co")]/@href').getall()
         links = ScrapymarketplaceItem()
-        links['links'] = arr
+        links['links'] = arr[:5]
         yield links
